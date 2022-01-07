53214d45d36a31f0339b7a348b42d7f5949af424
mafuyuan <937088546@qq.com>
2022-01-07 14:08:02 +0800

    first commit

---
+++ JCNetworking/Classes/JCNetworkManager.h
@@ -0,0 +1,18 @@
+//
+//  JCNetworkManager.h
+//  JCNetworking
+//
+//  Created by TOPBAND on 2022/1/7.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface JCNetworkManager : NSObject
+
+- (void)testPrint:(NSString *)text;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ JCNetworking/Classes/JCNetworkManager.m
@@ -0,0 +1,16 @@
+//
+//  JCNetworkManager.m
+//  JCNetworking
+//
+//  Created by TOPBAND on 2022/1/7.
+//
+
+#import "JCNetworkManager.h"
+
+@implementation JCNetworkManager
+- (void)testPrint:(NSString *)text {
+    
+    NSLog(@"JCNetworkManager print == %@",text);
+    
+}
+@end
